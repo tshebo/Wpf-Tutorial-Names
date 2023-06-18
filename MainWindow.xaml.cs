50a6d84e45f48112e303e943ce94995d5d21f948
tshebo <120386361+tshebo@users.noreply.github.com>
2023-06-18 20:07:12 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_Proj1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text)) //Checks if there is text inside my text box and check that the text does not exist in my listbox
+            {
+                lstNames.Items.Add(txtName.Text); //this adds the name to the list
+                txtName.Clear(); //this clears the name inside my txt box
+            }
+        }
+    }
+}
